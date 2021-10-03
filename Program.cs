1590be815c94ae93115155a51e4e00a90715f012
Brandon <bkuon@uco.edu>
2021-10-03 16:37:29 -0500

    Project Euler 1 .NET Code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Euler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 1000)
+                .Where(n => n % 3 == 0 || n % 5 == 0);
+
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
